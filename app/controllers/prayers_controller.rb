b72daf1c71b7e117b4ed4afc3bf4592823df35e1
mandolin <marktravismorales@gmail.com>
2016-11-16 19:05:58 +0000

    Complete prayer request function

--- app/controllers/prayers_controller.rb
+++ app/controllers/prayers_controller.rb
@@ -6,4 +6,17 @@ class PrayersController < ApplicationController
     def new
       @prayer = Prayer.new
     end
+    
+    def create
+      @prayer = Prayer.new(prayer_params)
+      @prayer.save
+      flash[:success] = "Prayer Request has been published"
+      redirect_to prayers_path
+    end
+    
+    private
+    
+    def prayer_params
+      params.require(:prayer).permit(:title, :body)
+    end
 end
